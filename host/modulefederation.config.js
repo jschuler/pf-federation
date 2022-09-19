f64b13bd6124b157da5c6d0ab53ba1a9af19dda4
Joachim Schuler <jdh.schuler@gmail.com>
2022-09-19 10:24:34 -0400

    host config

--- host/modulefederation.config.js
+++ host/modulefederation.config.js
@@ -4,10 +4,10 @@ const { dependencies } = require('./package.json');
 module.exports = {
   name: 'host',
   remotes: {
-    // remote_01: 'remote_01@http://localhost:3002/remoteEntry.js',
-    remote_01: 'remote_01@https://pf-css-modules-1.surge.sh/remoteEntry.js',
-    // remote_02: 'remote_02@http://localhost:3003/remoteEntry.js',
-    remote_02: 'remote_02@https://pf-css-modules-2.surge.sh/remoteEntry.js',
+    remote_01: 'remote_01@http://localhost:3002/remoteEntry.js',
+    // remote_01: 'remote_01@https://pf-css-modules-1.surge.sh/remoteEntry.js',
+    remote_02: 'remote_02@http://localhost:3003/remoteEntry.js',
+    // remote_02: 'remote_02@https://pf-css-modules-2.surge.sh/remoteEntry.js',
   },
   shared: {
     ...dependencies,
